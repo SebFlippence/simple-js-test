59756f327947e5f001b7f49b120b4650d029df36
Seb Flippence <144435+SebFlippence@users.noreply.github.com>
2011-06-10 16:37:20 +0000

    * Added optional fail message to assertions

--- simple-js-test/src/main/resources/javascript/assertions.js
+++ simple-js-test/src/main/resources/javascript/assertions.js
@@ -1,36 +1,32 @@
 
 function assertFail(msg) {
-    addMsg = "";
-    if (msg !== undefined) {
-        addMsg = ": " + msg;
-    }
-    throw "Assertion failed" + addMsg;
+    throw "Assertion failed" + ((msg !== undefined) ? ": " + msg : "");
 }
 
 function assertPass() {
     throw "mvn-js-test-[pass]";
 }
 
-function assertEquals(lhs, rhs) {
+function assertEquals(lhs, rhs, msg) {
     if (lhs != rhs) {
-        throw "Assertion failed: '" + lhs + "' != '" + rhs + "'";
+        throw "Assertion failed: '" + lhs + "' != '" + rhs + "'" + ((msg !== undefined) ? ": " + msg : "");
     }
 }
 
-function assertNotEquals(lhs, rhs) {
+function assertNotEquals(lhs, rhs, msg) {
     if (lhs == rhs) {
-        throw "Assertion failed: '" + lhs + "' == '" + rhs + "'";
+        throw "Assertion failed: '" + lhs + "' == '" + rhs + "'" + ((msg !== undefined) ? ": " + msg : "");
     }
 }
 
-function assertTrue(value) {
+function assertTrue(value, msg) {
     if (!value) {
-        throw "Assertion failed: '" + value + "' is not true";
+        throw "Assertion failed: '" + value + "' is not true" + ((msg !== undefined) ? ": " + msg : "");
     }
 }
 
-function assertFalse(value) {
+function assertFalse(value, msg) {
     if (value) {
-        throw "Assertion failed: '" + value + "' is not false";
+        throw "Assertion failed: '" + value + "' is not false" + ((msg !== undefined) ? ": " + msg : "");
     }
 }
